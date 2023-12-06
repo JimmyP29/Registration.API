34476cf4f5958744ffb726baa6a1fc5d344c31aa
James Privett <james.privett@feefo.com>
2023-12-06 14:46:34 +0000

    Removed commented User record

--- Registration.API/Registration.API/Program.cs
+++ Registration.API/Registration.API/Program.cs
@@ -31,16 +31,3 @@ app.MapGet("/users", () => {
 
 app.Run();
 
-/*public record User
-{
-    public string Username { get; set; }
-    public string Email { get; set; }
-    public string Password { get; set; }
-
-    public User(string username, string email, string password)
-    {
-        Username = username;
-        Email = email;
-        Password = password;
-    }
-}*/
