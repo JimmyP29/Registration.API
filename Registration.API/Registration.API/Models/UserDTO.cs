b82b0646ee0e351f631d783b7a62eb8580549228
James Privett <james.privett@feefo.com>
2023-12-06 15:12:16 +0000

    Made properties required

--- Registration.API/Registration.API/Models/UserDTO.cs
+++ Registration.API/Registration.API/Models/UserDTO.cs
@@ -2,9 +2,9 @@
 {
     internal class UserDTO
     {
-        public string Username { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
+        public required string Username { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
 
         public UserDTO(string username, string email, string password)
         {
