ec4d2b8d781b9c80830c5bf357c3c1ad0409d6ef
James Privett <james.privett@feefo.com>
2023-12-06 14:31:45 +0000

    register and users endpoints working as expected

--- Registration.API/Registration.API/Program.cs
+++ Registration.API/Registration.API/Program.cs
@@ -16,34 +16,29 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+var users = new List<User>();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
-app.MapGet("/blah", () =>
-{
-    return "blah";
+app.MapPost("/register", (User user) => {
+    users.Add(user);
 });
+
+app.MapGet("/users", () => {
+    return users;
+});
+
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+public record User
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+
+    public User(string username, string email, string password)
+    {
+        Username = username;
+        Email = email;
+        Password = password;
+    }
 }
